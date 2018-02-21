3c6e35f8c5846b77c4890a7b19967e59a274cd9d
Radgense <edgar.ochoa.c@gmail.com>
2018-02-21 01:32:37 +0100

    Adding link to host folder

--- provision/bootstrap.sh
+++ provision/bootstrap.sh
@@ -8,6 +8,10 @@ sudo apt-get -y install apache2
 # So we do not get "Could not reliably determine the server's fully qualified domain name" error
 echo "ServerName myVagrant" | sudo tee /etc/apache2/conf-available/servername.conf
 
+# Link /vagrant/html to /var/www/html
+sudo rm -rf /var/www/html
+sudo ln -s /vagrant/html /var/www
+
 # Restarting apache
 sudo service apache2 reload
 
