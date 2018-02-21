1ccad115c1349302680e4fd858f374e69f43d794
Radgense <edgar.ochoa.c@gmail.com>
2018-02-21 01:43:51 +0100

    Adding MySQL, Git and PHP installation

--- provision/bootstrap.sh
+++ provision/bootstrap.sh
@@ -2,8 +2,11 @@
 
 # Get updates
 sudo apt-get update
+
 # Install apache
 sudo apt-get -y install apache2
+sudo systemctl enable apache2
+sudo systemctl start apache2
 
 # So we do not get "Could not reliably determine the server's fully qualified domain name" error
 echo "ServerName myVagrant" | sudo tee /etc/apache2/conf-available/servername.conf
@@ -17,6 +20,14 @@ sudo service apache2 reload
 
 # Install git
 sudo apt-get -y install git
+
+# Install MySQL
+sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password root'
+sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password root'
+sudo apt-get -y install mysql-server libapache2-mod-auth-mysql php5-mysql
+
+# Install PHP
+sudo apt-get -y install php5 libapache2-mod-php5 php5-mcrypt
 
 # Print the VM IP address
 ip addr show eth1 | egrep "inet\ " | cut -f1 -d "/" | cut -f2 -d "t"
